fd10d5c4cf096b4434faea949a522d9aeb4adc44
airizar <airizar@gmail.com>
2015-03-17 19:22:50 +0100

    Ejemplo de cambios de estado y saveState

---
+++ ConfigChange/app/src/main/java/com/airizar/configchange/ActivityB.java
@@ -0,0 +1,123 @@
+package com.airizar.configchange;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class ActivityB extends ActionBarActivity {
+    private String data="data";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+        if(savedInstanceState!=null){
+            this.restoreState(savedInstanceState);
+        }
+        Button btnOpenA= (Button) findViewById(R.id.btnOpenA);
+        //addEventListener
+        btnOpenA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("CHANGE","Open Activity A from B");
+                //se crea la intencion de hacer algo
+                //aqui el this hace referencia al listener
+                //ActivityA.this: me devuelve la instancia de la actividad A
+                Intent openA= new Intent(ActivityB.this,ActivityA.class);
+                startActivity(openA);
+
+            }
+        });
+        Button btnClose= (Button) findViewById(R.id.btnClose);
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("CHANGE","Close B Activity");
+
+               //hace lo mismo que el volver : Stop y Destroy
+                finish();
+            }
+        });
+        Log.d("CHANGE", "ActivityB onCreate()");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("CHANGE", "ActivityB onStart()");
+    }
+    //stop y stop se paran servicios,..
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("CHANGE","ActivityB onStop()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("CHANGE","ActivityB onRestart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("CHANGE","ActivityB onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("CHANGE","ActivityB onPause()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("CHANGE","ActivityB onDestroy()");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        //bundle (cajon de sastre), se puede meter cualquier cosa
+        Log.d("CHANGE","Activity B onSaveInstanceState()");
+        super.onSaveInstanceState(outState);
+        outState.putString(data,"datos");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.d("CHANGE","Activity B onRestoreInstanceState()");
+        super.onRestoreInstanceState(savedInstanceState);
+        this.restoreState(savedInstanceState);
+    }
+    private void restoreState(Bundle savedInstaceState){
+        this.data=savedInstaceState.getString("data");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_activity_b, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
