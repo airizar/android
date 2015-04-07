79c8cb7d31fd983637b2b902f75f9b54c5f0370a
airizar <airizar@gmail.com>
2015-04-07 19:21:26 +0200

    Commit inicial de Geolocalizacion con Google Play Services

---
+++ GeolocationGPService/app/src/main/java/com/airizar/geolocationgpservice/MainActivity.java
@@ -0,0 +1,112 @@
+package com.airizar.geolocationgpservice;
+
+import android.location.Location;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
+
+
+public class MainActivity extends ActionBarActivity  implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener, LocationListener{
+    private GoogleApiClient googleApiClient;
+    private TextView lblEstado;
+    private TextView lblLatitud;
+    private TextView lblLongitud;
+    private Button btnLocActual;
+    private Button btnLocUpdate;
+    private GoogleApiClient mGoogleApiClient;
+    private boolean accesoGPS=false;
+    private boolean status=false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        prepararElementosVista();
+        prepararGPS();
+    }
+
+    private void prepararGPS() {
+        if(GooglePlayServicesUtil.isGooglePlayServicesAvailable(this)==ConnectionResult.SUCCESS){
+            accesoGPS=true;
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    private void prepararElementosVista() {
+        lblEstado=(TextView)findViewById(R.id.status);
+        lblLatitud=(TextView)findViewById(R.id.lblLat);
+        lblLongitud=(TextView)findViewById(R.id.lblLon);
+        btnLocActual=(Button)findViewById(R.id.btnLocActual);
+        btnLocUpdate=(Button)findViewById(R.id.btnLocUpdate);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(accesoGPS){
+            mGoogleApiClient.connect();
+        }
+    }
+
+    private void obtenerPosicionActual(){
+        
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+        @Override
+        public void onConnected(Bundle bundle) {
+            status=true;
+        }
+
+        @Override
+        public void onConnectionSuspended(int i) {
+
+        }
+
+        @Override
+        public void onConnectionFailed(ConnectionResult connectionResult) {
+
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            
+        }
+
+
+
+
+}
